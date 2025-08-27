5bb979883fffeff9978fe4accf3a9032367154d7
PINball1579 <usanakorn01@gmail.com>
2025-08-28 03:19:14 +0700

    Add Floating

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import About from './about/page'
 import RiskSection from './riskSection/page'
 import VaccineSection from './vaccineSection/page'
 import Footer from './footer/page'
+import Image from 'next/image'
 
 const HomePage: React.FC = () => {
   return (
@@ -13,7 +14,9 @@ const HomePage: React.FC = () => {
       <RiskSection />
       <VaccineSection />
       <Footer />
-      
+
+      {/* Floating Image */}
+      <Image src="/images/floatImage.png" alt="float" className='fixed bottom-4 right-4 z-50' width={290} height={250} />
     </div>
   )
 }
