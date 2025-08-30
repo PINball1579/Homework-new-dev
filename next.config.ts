dc55bcbeb355686cbae0fa8a0060b7c3f31c0cbd
PINball1579 <usanakorn01@gmail.com>
2025-08-30 16:49:31 +0700

    For deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+const repo = "Homework-new-dev";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: isProd ? `/${repo}` : "",
+  assetPrefix: isProd ? `/${repo}/` : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
